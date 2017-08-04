447f360ce30a6356b4f3164b094ba687c5505b8c
Gabrielle Ecanow <gecanow@gmail.com>
2017-08-03 19:48:30 -0500

    Trying to add in chat features...

---
+++ Study Buddy/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  Study Buddy
+//
+//  Created by Gaby Ecanow on 8/3/17.
+//  Copyright © 2017 Gaby Ecanow. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController {
+    
+    var ref: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
